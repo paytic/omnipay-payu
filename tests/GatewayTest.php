d4a94d9015393801e0b8d4648a0800d0994665c7
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-05-08 20:25:24 +0300

    initial commit

---
+++ tests/GatewayTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ByTIC\Omnipay\Payu\Tests;
+
+use ByTIC\Omnipay\Payu\Gateway;
+use ByTIC\Omnipay\Payu\Message\PurchaseRequest;
+
+/**
+ * Class HelperTest
+ * @package ByTIC\Omnipay\Payu\Tests
+ */
+class GatewayTest extends AbstractTest
+{
+    public function testGetSecureUrl()
+    {
+        $gateway = new Gateway();
+
+        // INITIAL TEST MODE IS TRUE
+        self::assertEquals(
+            'http://sandboxsecure.mobilpay.ro',
+            $gateway->getEndpointUrl()
+        );
+    }
+
+}
